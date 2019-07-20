8c3faf49e360695fe9cb609928ed09d8c3cde0f6
Asmita Vadage <asmitaborawake@gmail.com>
2019-07-20 15:35:52 +0530

    programatically create message app refer letsbuildapp

--- MessageAppDemo/ChatMessageCell.swift
+++ MessageAppDemo/ChatMessageCell.swift
@@ -9,16 +9,76 @@
 import UIKit
 
 class ChatMessageCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    
+    let messageLbl = UILabel()
+    let bublebackgroundView = UIView()
+    var leadingconstraint : NSLayoutConstraint!
+    var trillingconstraint : NSLayoutConstraint!
+    
+    var chatmessageset : chatMessage!{
+        didSet{
+            bublebackgroundView.backgroundColor = chatmessageset
+                .isIncoming ? .white : .darkGray
+            messageLbl.textColor = chatmessageset.isIncoming ? .black : .white
+            messageLbl.text = chatmessageset.text
+            
+            if chatmessageset.isIncoming {
+                trillingconstraint.isActive = false
+                leadingconstraint.isActive = true
+                
+            }else {
+                trillingconstraint.isActive = true
+                leadingconstraint.isActive = false
+            }
+            
+        }
     }
+    
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        
+        bublebackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(bublebackgroundView)
+        addSubview(messageLbl)
+        
+        bublebackgroundView.backgroundColor = .yellow
+        bublebackgroundView.layer.cornerRadius = 12
+        
+        messageLbl.numberOfLines = 0
+        messageLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        //set some constraints to lable
+        let constraints = [messageLbl.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+                        messageLbl.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+                        messageLbl.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
+            
+            bublebackgroundView.topAnchor.constraint(equalTo: messageLbl.topAnchor, constant: -16),
+            bublebackgroundView.leadingAnchor.constraint(equalTo: messageLbl.leadingAnchor, constant: -16),
+            bublebackgroundView.bottomAnchor.constraint(equalTo: messageLbl.bottomAnchor, constant: 16),
+            bublebackgroundView.trailingAnchor.constraint(equalTo: messageLbl.trailingAnchor, constant: 16),
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        leadingconstraint =  messageLbl.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32)
+        leadingconstraint.isActive = false
+        
+        trillingconstraint = messageLbl.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
+        trillingconstraint.isActive = true
+        
+       
     }
-
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
--- MessageAppDemo/ViewController.swift
+++ MessageAppDemo/ViewController.swift
@@ -8,13 +8,126 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct chatMessage {
+    let text : String
+    let isIncoming : Bool
+    let date : Date
+}
 
+extension Date {
+    static func dateFromCustomString(customString: String) -> Date {
+        
+        let dateformater = DateFormatter()
+        dateformater.dateFormat = "MM/dd/yyyy"
+        return dateformater.date(from: customString) ?? Date()
+       
+    }
+}
+
+class ViewController: UITableViewController {
+    fileprivate let cellid = "cell"
+    
+    let chatMessages = [
+        [
+            chatMessage(text:"here is my 1st text message", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018")),
+            chatMessage(text:"i am going to send long message that going to be word wrap", isIncoming: true, date: Date.dateFromCustomString(customString: "08/03/2018"))
+        ],
+        
+        [
+            chatMessage(text:"i am going to send long message that going to be word wrap and many more lines come, i am going to send long message that going to be word wrap and many more lines come hi ios devslopers", isIncoming: false , date: Date.dateFromCustomString(customString: "09/15/2018")),
+            chatMessage(text:"here is my 1st text message", isIncoming: false, date: Date.dateFromCustomString(customString: "09/15/2018"))
+        ],
+        
+        
+        [
+            chatMessage(text:"here is my 1st text message here is my 1st text message here is my 1st text message here is my 1st text message here is my 1st text message", isIncoming: true, date: Date.dateFromCustomString(customString: "10/31/2018")),
+            chatMessage(text:"here is my 1st text message", isIncoming: true, date: Date.dateFromCustomString(customString: "10/31/2018"))
+        ]
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles  = true
+        
+        tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellid)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return chatMessages.count
+    }
+    
+    
+    class dateHeaderLabel : UILabel{
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            backgroundColor = .green
+            textColor = .red
+            textAlignment = .center
+            translatesAutoresizingMaskIntoConstraints = false // enalbe auytolayout
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        override var intrinsicContentSize: CGSize{
+            font = UIFont.boldSystemFont(ofSize: 14)
+            let originalSize = super.intrinsicContentSize
+            let height = originalSize.height + 12
+            layer.cornerRadius = height / 2
+            layer.masksToBounds = true
+            
+            return CGSize(width: originalSize.width + 20, height: height)
+        }
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let firstMessageInSection = chatMessages[section].first{
+            
+            let dateformater = DateFormatter()
+            dateformater.dateFormat = "MM/dd/yyyy"
+            let dateString = dateformater.string(from: firstMessageInSection.date)
+            
+            
+            
+            let label = dateHeaderLabel()
+            label.text = dateString
+            
+            let contenerView = UIView()
+            contenerView.addSubview(label)
+            
+            label.centerXAnchor.constraint(equalTo: contenerView.centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: contenerView.centerYAnchor).isActive = true
+            
+            return contenerView
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath) as! ChatMessageCell
+        
+        let message = chatMessages[indexPath.section][indexPath.row]
+        cell.chatmessageset = message
+        
+        return cell
+    }
+    
+    
 }
 
